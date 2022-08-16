e5de63016d0ecc5313dfcef86e4dbc3e7d9dd8ea
rseeley42 <109935451+rseeley42@users.noreply.github.com>
2022-08-16 18:40:46 -0500

    updated files

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+alert('Hello world');
