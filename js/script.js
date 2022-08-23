d31e642fdf462cee1caacb9b7f1b589178d79b34
rseeley42 <109935451+rseeley42@users.noreply.github.com>
2022-08-23 10:30:21 -0500

    Update script.js
    added pokemon list

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-alert('Hello world');
-let favoriteFood = 'French Fries';
-document.write(favoriteFood);
+let pokemonList =[
+  {
+    name: 'Growlithe',
+    height: .7,
+    weight: 19,
+    types: ['fire']
+  },
+
+  {
+    name: 'Caterpie'
+    height: .3,
+    weight: 2.9,
+    types: ['bug']
+  },
+
+  {
+    name: 'Weedle',
+    height: .3,
+    weight: 3.2,
+    types: ['bug,' 'posion']
+  },
+]
