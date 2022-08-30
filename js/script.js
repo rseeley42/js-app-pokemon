3e2a69467c3273c5432c8788904f574f19d6313e
rseeley42 <109935451+rseeley42@users.noreply.github.com>
2022-08-30 10:27:36 -0500

    updates
    added loop and styling

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
-let pokemonList =[
-  {
-    name: 'Growlithe',
+let pokemonList = [
+  { name: 'Growlithe',
     height: .7,
     weight: 19,
-    types: ['fire']
+    types: 'fire'
   },
 
   {
-    name: 'Caterpie'
+    name: 'Caterpie',
     height: .3,
     weight: 2.9,
-    types: ['bug']
+    types: 'bug'
   },
 
   {
     name: 'Weedle',
     height: .3,
     weight: 3.2,
-    types: ['bug,' 'posion']
-  },
-]
+    types: 'bug'
+  }
+];
+
+for (let i = 0; i <= 2; i++) {
+  document.write('The Pokemon Name is: ');
+  document.write(pokemonList[i].name);
+  document.write("<br>");
+  document.write('Height:');
+  document.write(pokemonList[i].height);
+  document.write("<br>");
+  document.write('Weight:');
+  document.write(pokemonList[i].weight);
+  document.write("<br>");
+  if (pokemonList[i].height > .5) {document.write('Wow, that is big');}
+  document.write("<br>");
+  document.write("<br>");
+}
