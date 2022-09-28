579831021134abd4928abe8d2ce185ef6e914a50
rseeley42 <109935451+rseeley42@users.noreply.github.com>
2022-09-28 16:16:15 -0500

    deleted pokemon array

--- js/script.js
+++ js/script.js
@@ -1,25 +1,7 @@
 let pokemonRepository = (function () {
 let pokemonList = [
-  { name: 'Growlithe',
-    height: .7,
-    weight: 19,
-    types: 'fire'
-  },
-
-  {
-    name: 'Caterpie',
-    height: .3,
-    weight: 2.9,
-    types: 'bug'
-  },
-
-  {
-    name: 'Weedle',
-    height: .3,
-    weight: 3.2,
-    types: 'bug'
-  }
-];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
 /*Add a single pokemon to the list*/
 function add(pokemon) {
